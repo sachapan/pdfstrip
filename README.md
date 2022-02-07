ed56babb38a9e123377fc01133a2d52c559a6b64
Sacha Panasuik <sachapan@gmail.com>
2022-02-07 15:57:59 -0500

    Cleaned up some comments and updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 ## pdfstrip.py
 
 
-A little whacked together pdf processor using [pyPDF2](https://github.com/mstamy2/PyPDF2).
+A little whacked together pdf processor to delete pages from a pdf using [pyPDF2](https://github.com/mstamy2/PyPDF2).  It creates a new file and leaves the original intact.
 
+Sample run with a two page pdf.  Sample in this repo: [sachapan_github.pdf](https://github.com/sachapan/pdfstrip/sachapan_github.pdf).
+
+    python3 .\pdfstrip.py
+    Filename to strip: sachapan_github.pdf
+    -----------------------------------------------------------------------
+    sachapan_github.pdf  has  2  pages.
+    -----------------------------------------------------------------------
+    Pages to remove - space separated list: 2
+    Output will be sent to: sachapan_github_stripped.pdf
+    Processing.....
+    Deleting page:  2
+    sachapan_github_stripped.pdf strip completed.
+
+A larger sample file with 99 pages: issue [114 of the MagPi magazine](https://magpi.raspberrypi.com/issues/114/pdf/download).
+
+    python3 .\pdfstrip.py
+    Filename to strip: MagPi114.pdf
+    -----------------------------------------------------------------------
+    MagPi114.pdf  has  100  pages.
+    -----------------------------------------------------------------------
+    Pages to remove - space separated list: 2 4 7
+    Output will be sent to: MagPi114_stripped.pdf
+    Processing.....
+    Deleting pages:  [2, 4, 7]
+    Strip completed.
+    MagPi114_stripped.pdf has 97 pages.
 
 
--- pdfstrip.py
+++ pdfstrip.py
@@ -14,22 +14,29 @@ def main():
         else:
             print("No such file!")
     infile = PdfFileReader(input_file, 'rb')
-    #print("Number of pages in file: ", input_file)
     separator()
     print(input_file, " has ", infile.getNumPages(), " pages.")
     separator()
     input_string = input("Pages to remove - space separated list: ")
+
     output_file = (os.path.splitext(input_file)[0] + "_stripped.pdf")
     print("Output will be sent to: " + output_file)
     print("Processing.....")
     # Can we deal with comma separated automatically? 1, 3, 84
     # Can we deal with ranges? 7, 9-45
+    # if ',' in input_string:
+    #    pages_to_delete = list(map(int, input_string.split(',')))
+    #    print(pages_to_delete)
+    # exit()
+    # else:
     pages_to_delete = list(map(int, input_string.split()))
     if len(pages_to_delete) > 1:
         print("Deleting pages: ", str(pages_to_delete))
     else:
-        print("Deleting page: ", input_string[0])
+        print("Deleting page: ", int(pages_to_delete[0]))
+    # Need to decrement all pages to delete by one because PyPDF2 counts from zero.
     pages_to_delete = list(map(lambda x: x - 1, pages_to_delete))
+    #print("Transformed pages to delete: ", pages_to_delete)
     output = PdfFileWriter()
     for i in range(infile.getNumPages()):
         if i not in pages_to_delete:
@@ -37,7 +44,9 @@ def main():
             output.addPage(p)
     with open(output_file, 'wb') as f:
         output.write(f)
-    print(output_file, "strip completed.")
+    print("Strip completed.")
+    outfile = PdfFileReader(output_file, 'rb')
+    print(output_file, "has", outfile.getNumPages(), "pages.")
 
 
 if __name__ == "__main__":
